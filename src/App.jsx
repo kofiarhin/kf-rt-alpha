fa76752faeb1e22743a57506c3cf31a3a8d5c925
kofi arhin <kofiarhin69@gmail.com>
2025-05-19 11:33:05 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="heading">Hello World</h1>
+      <p className="slug">Watch this space</p>
+    </div>
+  );
+};
+
+export default App;
